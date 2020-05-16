2699e7d56ce491e03d9fd0f13af2ddf7a103927d
ali jalali <ali.5991.jalali@gmail.com>
2020-05-16 23:46:14 +0430

    Added digikala model

---
+++ models/Digikala.js
@@ -0,0 +1,11 @@
+// Required pakages
+const mongoose = require('mongoose');
+// Schema
+const Schema = mongoose.Schema;
+// schema
+const schema = new Schema({
+
+})
+// Model
+const model = mongoose.model('Digikala', schema, 'Digikala');
+module.exports = model;
