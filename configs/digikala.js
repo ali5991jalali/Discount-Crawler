99837a7c42f108c8864a0ab2c13aa2d9f71c2a18
ali jalali <ali.5991.jalali@gmail.com>
2020-05-20 21:26:29 +0430

    Added digikala configs

---
+++ configs/digikala.js
@@ -0,0 +1,92 @@
+module.exports = {
+    categories: {
+        'digital': {
+            categories: {
+                'mobileAccessories': {
+                    link: 'https://www.digikala.com/search/category-mobile-accessories/'
+                },
+                'mobile': {
+                    link: 'https://www.digikala.com/search/category-mobile-phone/'
+                },
+                'watch': {
+                    link: 'https://www.digikala.com/search/category-wearable-gadget/'
+                },
+                'headset': {
+                    link: 'https://www.digikala.com/search/category-headphone-headset-microphone/'
+                },
+                'speakerWireless&Wired': {
+                    link: 'https://www.digikala.com/search/category-speaker/'
+                },
+                'hard&flash': {
+                    link: 'https://www.digikala.com/search/category-data-storage/'
+                },
+                'camera': {
+                    link: 'https://www.digikala.com/search/category-camera/'
+                },
+                'cameraAccessories': {
+                    link: 'https://www.digikala.com/search/category-camera-accessories/'
+                },
+                'Binoculars': {
+                    link: 'https://www.digikala.com/search/category-binoculars/'
+                },
+                'xbox&ps&game': {
+                    link: 'https://www.digikala.com/search/category-game-console/'
+                },
+                'computer&accessories': {
+                    link: 'https://www.digikala.com/search/category-computer-parts/'
+                },
+                'laptop': {
+                    link: 'https://www.digikala.com/search/category-notebook-netbook-ultrabook/'
+                },
+                'laptopAccessories': {
+                    link: 'https://www.digikala.com/search/category-laptop-accessories/'
+                },
+                'tablet': {
+                    link: 'https://www.digikala.com/search/category-tablet/'
+                },
+                'mobile&tabletCharger': {
+                    link: 'https://www.digikala.com/search/category-car-charger/'
+                },
+                'tabletAccessories': {
+                    link: 'https://www.digikala.com/search/category-tablet-accessories/'
+                },
+
+            }
+        },
+        'cosmetic&health': {
+            categories: {
+
+            }
+        },
+        'tool&car&office': {
+            categories: {
+
+            }
+        },
+        'mode&fashion': {
+            categories: {
+
+            }
+        },
+        'house&kitchen': {
+            categories: {
+
+            }
+        },
+        'book&art': {
+            categories: {
+
+            }
+        },
+        'toy&children': {
+            categories: {
+
+            }
+        },
+        'sport&travel': {
+            categories: {
+
+            }
+        }
+    }
+}
--- functions/digikala.js
+++ functions/digikala.js
@@ -2,6 +2,19 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
 
+// Persian number converter
+const
+    persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g],
+    arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g],
+    fixNumbers = function (str) {
+        if (typeof str === 'string') {
+            for (var i = 0; i < 10; i++) {
+                str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
+            }
+        }
+        return str;
+    };
+
 module.exports = {
     async getPageDiscounts(url) {
         try {
@@ -16,19 +29,21 @@ module.exports = {
                 const result = {};
                 let percent = (element.children[0].children[0].data).trim().replace('٪', '');
                 let lastPrice = element.parentNode.children[0].children[0].data.trim();
-                let activePrice = element.next.children[0].data.trim()
+                let activePrice = element.next.children[0].data.trim();
                 let data = ($(element).parents('li')[0].children[0].attribs)
-                console.log(($($(element).parents('.c-product-box__content')[0].prev).attr('href')))
+                let link = `https://digikala.com${($($(element).parents('.c-product-box__content')[0].prev).attr('href')).split('?')[0]}`;
                 let image = ($($(element).parents('.c-product-box__content')[0].prev).find('img')[0].attribs)
                 Object.assign(result, {
                     id: data['data-id'],
                     titleFa: data['data-title-fa'],
                     titleEn: data['data-title-en'],
-                    lastPrice,
-                    activePrice,
-                    percent,
-                    image: image['src']
+                    lastPrice: fixNumbers(lastPrice),
+                    activePrice: fixNumbers(activePrice),
+                    percent: fixNumbers(percent),
+                    image: image['src'],
+                    link
                 })
+                console.log(result)
                 pageResult.push(result)
             })
             return pageResult;
